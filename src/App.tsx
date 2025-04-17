a2d697690ae13c026fe3418709a19edb1ccedb2d
dmihovch <dmihovch@gmail.com>
2025-04-17 16:33:52 -0400

    setup routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
 import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import NotFoundPage from "./pages/NotFoundPage";
+import About from "./pages/About";
 
 function App() {
   return (
-    <>
-      <h1>Hello</h1>
-    </>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/about" element={<About />} />
+      <Route path="*" element={<NotFoundPage />} />
+    </Routes>
   );
 }
 
